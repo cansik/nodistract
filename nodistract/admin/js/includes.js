e9197b7e65d6215efdfe3710962aeb22dffa765d
Niore <julzioerjen@gmail.com>
2016-06-14 14:09:48 +0200

    Update Js

--- nodistract/admin/js/includes.js
+++ nodistract/admin/js/includes.js
@@ -1,5 +1,5 @@
 // Constant for API
-var apiBaseUrl = 'http://easyguet.ch/nodistract/public/api/';
+var apiBaseUrl = 'https://easyguet.ch/nodistract/public/api/';
 // Constant for Redirecturl after logout
 var redirectUrl = 'https://easyguet.ch/nodistract/public/';
 // Constant for Spell-API
