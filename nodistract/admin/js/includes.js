e7af9e03c67bac3451264608a88e23e45be21e59
Florian Bruggisser <florian@nexpose.ch>
2016-10-07 11:45:58 +0200

    moved blog files to root folder

--- nodistract/admin/js/includes.js
+++ nodistract/admin/js/includes.js
@@ -1,7 +1,7 @@
 // Constant for API
-var apiBaseUrl = 'https://easyguet.ch/nodistract/public/api/';
+var apiBaseUrl = 'https://easyguet.ch/nodistract/api/';
 // Constant for Redirecturl after logout
-var redirectUrl = 'https://easyguet.ch/nodistract/public/';
+var redirectUrl = 'https://easyguet.ch/nodistract/';
 // Constant for Spell-API
 var spellChecker = 'https://bingapis.azure-api.net/api/v5/spellcheck?mode=spell';
 // array for images
--- nodistract/index.php
+++ nodistract/index.php
@@ -25,27 +25,27 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 
 }
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 // Models
-require "../models/image.php";
-require "../models/post.php";
-require "../models/user.php";
+require "models/image.php";
+require "models/post.php";
+require "models/user.php";
 
 session_start();
 
 // Instantiate the app
-$settings = require __DIR__ . '/../src/settings.php';
+$settings = require __DIR__ . '/src/settings.php';
 $app = new \Slim\App($settings);
 
 // Set up dependencies
-require __DIR__ . '/../src/dependencies.php';
+require __DIR__ . '/src/dependencies.php';
 
 // Register middleware
-require __DIR__ . '/../src/middleware.php';
+require __DIR__ . '/src/middleware.php';
 
 // Register routes
-require __DIR__ . '/../src/routes.php';
+require __DIR__ . '/src/routes.php';
 
 // Run app
 $app->run();
